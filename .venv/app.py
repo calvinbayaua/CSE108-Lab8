dd8add67f73826b4b661e6cb7863305b93402992
rooja13 <rbarajas1633@yahoo.com>
2024-04-05 16:00:45 -0700

    Added Admin page and simple student table
    To do:
    design and implement database relationships

--- .venv/app.py
+++ .venv/app.py
@@ -6,9 +6,20 @@ import json
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
 db = SQLAlchemy(app)
+admin = Admin(app)
 
+#This table will be student view of classes
+class Student_table(db.Model):
+    courseName = db.Column(db.String, primary_key=True)
+    teacher = db.Column(db.String, unique=True, nullable=False)
+    time = db.Column(db.String, unique=True, nullable=False)
+    
+    #This could be pulled from a different table
+    #enrollment = db.Column(db.String, unique=True, nullable=False)
+    
 @app.route('/')
 def start():
     db.create_all()
