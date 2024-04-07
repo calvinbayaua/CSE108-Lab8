9986e9d91698ecd3d4b9cb195bc6fdcf8b1bf5d8
GaigeBonderer <gbonderer@ucmerced.edu>
2024-04-06 23:46:14 -0700

    Included code from Calvin and Roger
    I included the code Roger and Calvin have already created.

---
+++ .venv/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+import json
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+db = SQLAlchemy(app)
+admin = Admin(app)
+
+#This table will be student view of classes
+class Student_table(db.Model):
+    courseName = db.Column(db.String, primary_key=True)
+    teacher = db.Column(db.String, unique=True, nullable=False)
+    time = db.Column(db.String, unique=True, nullable=False)
+    
+    #This could be pulled from a different table
+    #enrollment = db.Column(db.String, unique=True, nullable=False)
+    
+@app.route('/')
+def start():
+    db.create_all()
+    return render_template("login.html") 
+
+if __name__ == "__main__":
+    app.run()
